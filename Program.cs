73518d5cfa360f1c435b2fc61ac6f00cdf21a061
nicholas harding <hardingnick00@gmail.com>
2021-04-10 17:02:32 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraticalTask3_LinearAndBinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
